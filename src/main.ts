b5ecf007e2224dcb22c4ca280337a2679d18646e
ranyery <ranyerysc@gmail.com>
2023-03-30 10:09:22 -0300

    refactor: adjusted vercel analytics script

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { inject as injectVercelAnalytics } from '@vercel/analytics';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+  injectVercelAnalytics();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
